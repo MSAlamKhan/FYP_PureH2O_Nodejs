ab6dad22daf2f12e01f3aa6c9e8444e22df3d788
MShahbazAlamKhan <100859403+MSAlamKhan@users.noreply.github.com>
2024-02-16 00:45:39 +0500

    Database Connection Configured

--- Database/DBonnection.js
+++ Database/DBonnection.js
@@ -1,16 +1,20 @@
 const SQL = require('mysql')
 
 const Database = SQL.createConnection({
-    host: "http://fyp-database.cfesa00gm51z.eu-north-1.rds.amazonaws.com",
-    port:3306,
+    host: "fyp-database.cfesa00gm51z.eu-north-1.rds.amazonaws.com",
+    port: 3306,
     user: "PUREH2O",
     password: "Pureh20!",
+    database:"PureH2O"
 });
 
-// Database.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
+// Database.connect(function (err) {
+//     if (err){ console.log(err) }
+//     else{
+//         console.log("Connected!");
+//     }
+    
+// });
 
 
 module.exports = Database
