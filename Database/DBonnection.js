9a77dbd25ca6a34a0b0f0e9c1807d4b45ab13dc8
MShahbazAlamKhan <100859403+MSAlamKhan@users.noreply.github.com>
2024-02-15 23:14:41 +0500

    revert back

--- Database/DBonnection.js
+++ Database/DBonnection.js
@@ -1,7 +1,8 @@
 const SQL = require('mysql')
 
 const Database = SQL.createConnection({
-    host: "http://fyp-database.cfesa00gm51z.eu-north-1.rds.amazonaws.com:3306",
+    host: "http://fyp-database.cfesa00gm51z.eu-north-1.rds.amazonaws.com",
+    port:3306,
     user: "PUREH2O",
     password: "Pureh20!",
 });
